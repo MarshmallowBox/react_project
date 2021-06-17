06a8fca8d9b2de30bf9b8b7661d674b896879a26
EC2 Default User <ec2-user@ip-172-31-47-31.ap-northeast-2.compute.internal>
2021-06-17 04:34:32 +0000

    [#10] Container, API, modules 구현

---
+++ frontend/src/containers/post/PostViewerContainer.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { readPost, unloadPost } from '../../modules/post';
+import PostViewer from '../../components/post/PostViewer';
+
+const PostViewerContainer = ({ match }) => {
+  // 처음 마운트 될 때 포ㅅ트 읽기 API 요청
+  const { postId } = match.params;
+  console.log(postId);
+  const dispatch = useDispatch();
+  const { post, error, loading } = useSelector (({ post, loading }) => ({
+    post: post.post,
+    error: post.error,
+    loading: loading['post/Read_post'],
+  }));
+  
+  useEffect(() => {
+    dispatch(readPost(postId));
+    // post 페이지를 나갈 때 initialState를 넣어준다
+    return () => {
+      dispatch(unloadPost());
+    }
+  }, [dispatch, postId]);
+  
+  return <PostViewer post={post} loading={loading} error={error} />;
+}
+
+export default withRouter(PostViewerContainer);
