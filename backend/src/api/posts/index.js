1d4cf707dd6ed344d70fba8f65836693fcd4ff24
EC2 Default User <ec2-user@ip-172-31-47-31.ap-northeast-2.compute.internal>
2021-07-06 12:19:21 +0000

    [#12] post 수정 frontend

--- backend/src/api/posts/index.js
+++ backend/src/api/posts/index.js
@@ -4,7 +4,7 @@ import checkLoggedIn from '../../lib/checkLoggedIn.js';
 const posts = new Router();
 const post = new Router();
 
-posts.get('/', postsCtrl.list);
+posts.get('/', checkLoggedIn, postsCtrl.list);
 posts.post('/', checkLoggedIn, postsCtrl.write);
 
 // api/posts/:id
--- backend/src/lib/checkLoggedIn.js
+++ backend/src/lib/checkLoggedIn.js
@@ -3,6 +3,7 @@ const checkLoggedIn = (ctx, next) => {
   if (!ctx.state.user) {
     // unauthorize
     ctx.status = 401;
+    // ctx.redirect('/login');
     return;
   }
   return next();
